c32156644be01524f739ab0bf84b47bb8178fdb1
Jorge Avila <avilajorge314@gmail.com>
2019-11-04 10:51:49 -0800

    Getting ready for deployment part 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+    FirebaseAppProvider,
+    AuthCheck,
+    SuspenseWithPerf, useFirebaseApp
+} from 'reactfire';
+import 'firebase/performance';
+import firebase from 'firebase';
+// Bootstrap
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
+// import Home from './home/home';
+import App from './App';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASE_m6wDIaaXit-Wh-jWXOobBjBgEjEsg",
+    authDomain: "nutab-jorge.firebaseapp.com",
+    databaseURL: "https://nutab-jorge.firebaseio.com",
+    projectId: "nutab-jorge",
+    storageBucket: "nutab-jorge.appspot.com",
+    messagingSenderId: "723291823243",
+    appId: "1:723291823243:web:b55fcac31bf162b75ec88e",
+    measurementId: "G-BXB4HB9692"
+};
+
+const timerCollectionName = 'countdown-timers';
+
+function UseCallFirebase(props) {
+
+    console.log("UseCallFirebase called!");
+
+    const firebaseApp = useFirebaseApp();
+    const ref = firebaseApp.firestore().collection(timerCollectionName);
+
+    return (
+        <App timerRef={ref}/>
+    );
+}
+
+function Login(props) {
+    console.log(props);
+    let auth = () => {
+        firebase.auth().getRedirectResult().then((result) => {
+            let token = null;
+            if (result.credential) {
+                token = result.credential.accessToken;
+            }
+            let user = result.user;
+        });
+        let provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('profile');
+        provider.addScope('email');
+        firebase.auth().signInWithRedirect(provider);
+    };
+
+    return (
+        <Button variant="light" size="lg" className="buttons" onClick={auth}>
+            <FontAwesomeIcon icon={faGoogle}/>
+        </Button>
+    );
+}
+
+
+
+
+ReactDOM.render(
+    <FirebaseAppProvider firebaseConfig={firebaseConfig} initPerformance>
+        <SuspenseWithPerf traceId={'load-login-status'} fallback={<p>Loading Login Status ....</p>}>
+            <AuthCheck fallback={<Login/>}>
+                <UseCallFirebase/>
+            </AuthCheck>
+        </SuspenseWithPerf>
+    </FirebaseAppProvider>
+    ,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
