e60756fbc4e7d482a8521b6f24010e84b23d38dd
Jorge Avila <avilajorge314@gmail.com>
2019-11-06 22:13:46 -0800

    Added some formatting to cse224 page.

--- src/cse224/CSE224.js
+++ src/cse224/CSE224.js
@@ -8,11 +8,11 @@ const CSE224 = React.forwardRef((props, ref) => {
     return (
         <>
             <div ref={ref}>
-                <blockquote className="trello-card">
-                    <a href="https://trello.com/c/ov9vWvYh/201-project-2">Project 2</a>
-                </blockquote>
                 <div className="jumbotron bg-white" >
-                    <iframe id="cse224-cal" src="https://cseweb.ucsd.edu/~gmporter/classes/fa19/cse224/index.html" width="100%" height="1400" />
+                    <blockquote className="trello-card">
+                        <a href="https://trello.com/c/ov9vWvYh/201-project-2">Project 2</a>
+                    </blockquote>
+                    <iframe style={{marginTop: '2rem'}} id="cse224-cal" src="https://cseweb.ucsd.edu/~gmporter/classes/fa19/cse224/index.html" width="100%" height="1400" />
                 </div>
             </div>
         </>
