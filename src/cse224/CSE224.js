b7e98f0ed952e04d00f50b6ed4d9d2e9a63143ba
Jorge Avila <avilajorge314@gmail.com>
2019-11-06 21:59:54 -0800

    Added CSE224 iframe

--- src/cse224/CSE224.js
+++ src/cse224/CSE224.js
@@ -11,8 +11,8 @@ const CSE224 = React.forwardRef((props, ref) => {
                 <blockquote className="trello-card">
                     <a href="https://trello.com/c/ov9vWvYh/201-project-2">Project 2</a>
                 </blockquote>
-                <div className="jumbotron bg-white" id="cse224-cal" >
-                    <iframe id="cse224-cal" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQGdRBOtf9x-RkvHstPdIcJpR3oKhWAQkk2pzsbSAEVDgFX78Vszy0Rk1D7dCuJDTleI8b1HdyH4m9I/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" width="100%" height="1400"></iframe>
+                <div className="jumbotron bg-white" >
+                    <iframe id="cse224-cal" src="http://cseweb.ucsd.edu/~gmporter/classes/fa19/cse224/index.html" width="100%" height="1400" />
                 </div>
             </div>
         </>
